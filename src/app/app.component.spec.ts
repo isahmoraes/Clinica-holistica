0f913dc21431153744db09c39e1cdba54f363950
Isadora-Moraes <167359026+Isadora-Moraes@users.noreply.github.com>
2024-09-22 22:45:25 -0300

    Estrutura limpa e adicionando arquivos novamente

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'clinica_holistica_new' title`, () => {
+  it(`should have the 'clinica_holistica' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('clinica_holistica_new');
+    expect(app.title).toEqual('clinica_holistica');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, clinica_holistica_new');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, clinica_holistica');
   });
 });
